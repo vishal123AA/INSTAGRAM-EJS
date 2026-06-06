08a81bad86fc8173023501ec6c60cfcb02e86047
vishal <vishalkumarkalher16@gmail.com>
2026-06-06 15:29:34 +0530

    changes done in index.js

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const app = express();
 const PORT = 8080;
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/ig/:username", (req, res) => {
     let { username } = req.params;
-    res.render("instagram.ejs", { username });});
+    let instaData = require("./data.json");
+    let data = instaData[username];
+    if (data){
+    res.render("instagram.ejs", { data });}
+    else{
+        res.render("error.ejs", { username });
+    }}
+);
 
 app.listen(PORT, () => {
-    console.log(`Server is running${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
